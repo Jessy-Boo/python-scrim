6807907d633468d9259767c7f0e4fb977cb1825e
Jessy <41843357+FurtivePhallus@users.noreply.github.com>
2018-11-18 02:25:34 -0700

    Add files via upload

--- neweggscrape.py
+++ neweggscrape.py
@@ -1,9 +1,11 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+
 my_url = 'https://www.newegg.ca/Video-Cards-Video-Devices/Category/ID-38'
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
+
 page_soup = soup(page_html, "html.parser")
 containers = page_soup.findAll("div",{"class":"item-container"})
 
@@ -19,5 +21,3 @@ for container in containers:
 	shipping = shipping_container[0].text.strip()
 	f.write(product_name.replace(",","|") + "," + shipping + "\n")
 f.close()
-	#print("Product: "+product_name)
-	#print("Shipping: "+shipping)
